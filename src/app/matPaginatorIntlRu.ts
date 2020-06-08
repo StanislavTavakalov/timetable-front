6f78bd476f0f57efd9a9cca980d3f2d90b148cab
Stas <stavakalov@inbox.ru>
2020-06-09 02:33:15 +0300

    paginator range fix

--- src/app/matPaginatorIntlRu.ts
+++ src/app/matPaginatorIntlRu.ts
@@ -1,5 +1,6 @@
 import {MatPaginatorIntl} from '@angular/material';
 import {Injectable} from '@angular/core';
+
 @Injectable()
 export class MatPaginatorIntlRu extends MatPaginatorIntl {
   itemsPerPageLabel = 'Записей на странице';
@@ -7,5 +8,11 @@ export class MatPaginatorIntlRu extends MatPaginatorIntl {
   previousPageLabel = 'Предыдущая страница';
   lastPageLabel = 'Последняя страница';
   firstPageLabel = 'Первая страница';
+  getRangeLabel = (page: number, pageSize: number, length: number): string => {
+    length = Math.max(length, 0);
+    const startIndex = page * pageSize;
+    const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+    return startIndex + 1 + ' — ' + endIndex + ' из ' + length;
+  };
 
 }
